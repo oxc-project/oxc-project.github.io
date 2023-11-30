20fefdacf6290580aefecd7f0f3c6c0d965df448
Nozomu Ikuta <16436160+NozomuIkuta@users.noreply.github.com>
2023-12-01 00:59:57 +0900

    chore(core): refine locale config (#39)

---
+++ .vitepress/locales/index.ts
@@ -0,0 +1,55 @@
+import consola from "consola";
+import type { DefaultTheme, HeadConfig, LocaleConfig, LocaleSpecificConfig } from "vitepress";
+
+type Config = LocaleConfig<DefaultTheme.Config>[string] & {
+  lang: NonNullable<LocaleSpecificConfig<DefaultTheme.Config>["lang"]>;
+  title: NonNullable<LocaleSpecificConfig<DefaultTheme.Config>["title"]>;
+  titleTemplate: NonNullable<LocaleSpecificConfig<DefaultTheme.Config>["titleTemplate"]>;
+  description: NonNullable<LocaleSpecificConfig<DefaultTheme.Config>["description"]>;
+  themeConfig: NonNullable<LocaleSpecificConfig<DefaultTheme.Config>["themeConfig"]>;
+};
+
+export function defineLocaleConfig(key: string, config: Config): LocaleConfig<DefaultTheme.Config> {
+  const { title, description, head = [] } = config;
+
+  const newHead: HeadConfig[] = [
+    ["meta", { property: "og:title", content: title }],
+    ["meta", { property: "og:description", content: description }],
+    ["meta", { name: "twitter:title", content: title }],
+    ["meta", { name: "twitter:description", content: description }],
+    ["meta", { name: "twitter:image:alt", content: title }],
+  ];
+
+  head.forEach(([tag, attrs]) => {
+    if (tag !== "meta") {
+      return;
+    }
+
+    let propertyOrName: string | null = null;
+
+    if (attrs.property === "og:title" || attrs.property === "og:description") {
+      propertyOrName = attrs.property;
+    } else if (
+      attrs.name === "twitter:title" ||
+      attrs.name === "twitter:description" ||
+      attrs.name === "twitter:image:alt"
+    ) {
+      propertyOrName = attrs.name;
+    }
+
+    if (propertyOrName === null) {
+      newHead.push([tag, attrs]);
+      return;
+    }
+
+    const error = new Error(
+      `Do not set \`${propertyOrName}\` in locale-level \`head\` config, because it is supposed to be set automatically according to \`title\` and \`description\` options.`,
+    );
+    consola.error(error);
+    throw error;
+  });
+
+  config.head = newHead;
+
+  return { [key]: config };
+}
