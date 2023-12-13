17cea316c53a415cc74149602913fe2823844d5d
Boshen <boshenc@gmail.com>
2023-12-13 11:00:44 +0800

    docs(core): hide i18n because they are not ready

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -2,14 +2,15 @@ import { defineConfig } from "vitepress";
 
 import { sharedConfig } from "./shared";
 import { enConfig } from "./en";
-import { zhConfig } from "./zh";
-import { jaConfig } from "./ja";
+// import { zhConfig } from "./zh";
+// import { jaConfig } from "./ja";
 
 export default defineConfig({
   ...sharedConfig,
   locales: {
     ...enConfig,
-    ...zhConfig,
-    ...jaConfig,
+    // Let's enable i18n when they are ready, i.e. most of the translations are done.
+    // ...zhConfig,
+    // ...jaConfig,
   },
 });
