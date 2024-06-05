688b31e6faa44943327b10ae4ae67e61aa769d7e
Boshen <boshenc@gmail.com>
2024-06-05 21:48:03 +0800

    Revert "Revert "redirect to oxc.rs""
    This reverts commit fa03234a0f23832e17d4f100cafd5e13fece471c.

--- .vitepress/config/shared.ts
+++ .vitepress/config/shared.ts
@@ -3,50 +3,59 @@ import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitepress";
 import svgLoader from "vite-svg-loader";
 
+const head = [
+  [
+    "link",
+    {
+      rel: "icon",
+      href: "https://cdn.jsdelivr.net/gh/oxc-project/oxc-assets/logo-round-min.png",
+    },
+  ],
+  // Open Graph
+  ["meta", { property: "og:site_name", content: "Oxc" }],
+  ["meta", { property: "og:url", content: "https://github.com/oxc-project" }],
+  [
+    "meta",
+    {
+      property: "og:image",
+      content: "https://cdn.jsdelivr.net/gh/oxc-project/oxc-assets/preview-white-min.png",
+    },
+  ],
+  // Twitter (X)
+  ["meta", { name: "twitter:site", content: "Oxc" }],
+  ["meta", { name: "twitter:card", content: "summary_large_image" }],
+  [
+    "meta",
+    {
+      name: "twitter:image",
+      content: "https://cdn.jsdelivr.net/gh/oxc-project/oxc-assets/preview-white-min.png",
+    },
+  ],
+  // Google Analytics
+  ["script", { async: "", src: "https://www.googletagmanager.com/gtag/js?id=G-X7WQ091KL7" }],
+  [
+    "script",
+    {},
+    `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-X7WQ091KL7');`,
+  ],
+];
+
+if (process.env.NODE_ENV === "production") {
+  head.unshift(
+    ["meta", { "http-equiv": "refresh", content: "0; URL=https://oxc.rs" }],
+    ["link", { rel: "canonical", href: 'href="https://oxc.rs"' }],
+  );
+}
+
 export const sharedConfig = defineConfig({
   srcDir: "src",
   srcExclude: [],
   outDir: "build",
   base: "/",
-  head: [
-    [
-      "link",
-      {
-        rel: "icon",
-        href: "https://cdn.jsdelivr.net/gh/oxc-project/oxc-assets/logo-round-min.png",
-      },
-    ],
-    // Open Graph
-    ["meta", { property: "og:site_name", content: "Oxc" }],
-    ["meta", { property: "og:url", content: "https://github.com/oxc-project" }],
-    [
-      "meta",
-      {
-        property: "og:image",
-        content: "https://cdn.jsdelivr.net/gh/oxc-project/oxc-assets/preview-white-min.png",
-      },
-    ],
-    // Twitter (X)
-    ["meta", { name: "twitter:site", content: "Oxc" }],
-    ["meta", { name: "twitter:card", content: "summary_large_image" }],
-    [
-      "meta",
-      {
-        name: "twitter:image",
-        content: "https://cdn.jsdelivr.net/gh/oxc-project/oxc-assets/preview-white-min.png",
-      },
-    ],
-    // Google Analytics
-    ["script", { async: "", src: "https://www.googletagmanager.com/gtag/js?id=G-X7WQ091KL7" }],
-    [
-      "script",
-      {},
-      `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-X7WQ091KL7');`,
-    ],
-  ],
+  head,
   lastUpdated: false,
   themeConfig: {
     siteTitle: "Oxc",
