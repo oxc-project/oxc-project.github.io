6728013e0aa869dc755f37f0fb3c0766e86ccf0a
Don Isaac <donald.isaac@gmail.com>
2024-08-11 23:54:56 -0400

    feat(theme): add Alert component (#169)

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,12 +3,14 @@ import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import AppBadgeList from "./components/AppBadgeList.vue";
 import AppBlogPostHeader from "./components/AppBlogPostHeader.vue";
+import Alert from "./components/Alert.vue";
 
 export default {
   extends: DefaultTheme,
   async enhanceApp({ app }) {
     app.component("AppBadgeList", AppBadgeList);
     app.component("AppBlogPostHeader", AppBlogPostHeader);
+    app.component("Alert", Alert);
   },
   // Layout() {
   // return h(DefaultTheme.Layout, null, {
