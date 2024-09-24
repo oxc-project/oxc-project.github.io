0067dc255dec1dd3d2d5c2fcdcf50aaee5c2dd13
Boshen <boshenc@gmail.com>
2024-09-24 19:40:37 +0800

    Oxc Transformer Alpha

--- .vitepress/config/en.ts
+++ .vitepress/config/en.ts
@@ -23,7 +23,7 @@ export const enConfig = defineLocaleConfig("root", {
       {
         text: "Resources",
         items: [
-          { text: "Blog", link: "/blog/2024-05-04-import-plugin-alpha.html" },
+          { text: "Blog", link: "/blog/2024-09-29-transformer-alpha" },
           { text: "Team", link: "/team" },
           { text: "Branding", link: "/branding" },
           { text: "Website GitHub", link: "https://github.com/oxc-project/oxc-project.github.io" },
