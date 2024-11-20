3a4a2b888482389f8f517171d725c561233be990
no-yan <63000297+no-yan@users.noreply.github.com>
2024-11-20 19:34:28 +0900

    chore(blog): update blog link in header component (#244)

--- .vitepress/config/en.ts
+++ .vitepress/config/en.ts
@@ -23,7 +23,7 @@ export const enConfig = defineLocaleConfig("root", {
       {
         text: "Resources",
         items: [
-          { text: "Blog", link: "/blog/2024-09-29-transformer-alpha" },
+          { text: "Blog", link: "/blog/2024-10-18-oxlint-v0.10-release" },
           { text: "Team", link: "/team" },
           { text: "Branding", link: "/branding" },
           { text: "Website GitHub", link: "https://github.com/oxc-project/oxc-project.github.io" },
