29fec35015e2462ee0fb032dd23ece25abb102ca
Boshen <boshenc@gmail.com>
2025-03-14 12:29:42 +0800

    update

--- .vitepress/config/en.ts
+++ .vitepress/config/en.ts
@@ -23,7 +23,7 @@ export const enConfig = defineLocaleConfig("root", {
       {
         text: "Resources",
         items: [
-          { text: "Blog", link: "/blog/2025-03-13-minifier-alpha" },
+          { text: "Blog", link: "/blog/2025-03-15-oxlint-beta" },
           { text: "Team", link: "/team" },
           { text: "Branding", link: "/branding" },
           { text: "Website GitHub", link: "https://github.com/oxc-project/oxc-project.github.io" },
