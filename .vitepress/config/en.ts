a5d3ba486646f8081572662ea89489d9ecb66c53
Jerren T <JerrenT@Trifall.com>
2025-12-08 20:30:02 -0500

    Update blog link in resources section (#692)
    Resources -> blog link doesn't resolve to a valid blog URL currently.
    
    Updates this to the latest blog post as of now.
    
    Signed-off-by: Jerren T <JerrenT@Trifall.com>

--- .vitepress/config/en.ts
+++ .vitepress/config/en.ts
@@ -23,7 +23,7 @@ export const enConfig = defineLocaleConfig("root", {
       {
         text: "Resources",
         items: [
-          { text: "Blog", link: "/blog/2025-12-08-oxfmt-alpha" },
+          { text: "Blog", link: "/blog/2025-12-08-type-aware-alpha" },
           { text: "Team", link: "/team" },
           { text: "Release Notes", link: "https://github.com/oxc-project/oxc/releases" },
           { text: "Branding", link: "/branding" },
