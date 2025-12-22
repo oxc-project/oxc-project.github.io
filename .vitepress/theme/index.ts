c9a5970483af2ad89b01011a44ca72cd011fc6e4
marchantweb <simon@marchantweb.com>
2025-12-22 16:15:58 -0500

    feat: clean up

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,22 +1,16 @@
 import "virtual:group-icons.css";
 import type { Theme } from "vitepress";
-import VoidZeroTheme from '@voidzero-dev/vitepress-theme'
-import '@voidzero-dev/vitepress-theme/index.css'
+import VoidZeroTheme from "@voidzero-dev/vitepress-theme";
+import "@voidzero-dev/vitepress-theme/index.css";
 import { defineAsyncComponent, h } from "vue";
-import Alert from "./components/Alert.vue";
-import AppBadgeList from "./components/AppBadgeList.vue";
 import AppBlogPostHeader from "./components/AppBlogPostHeader.vue";
 
 export default {
   extends: VoidZeroTheme,
   async enhanceApp({ app }) {
-    app.component("AppBadgeList", AppBadgeList);
     app.component("AppBlogPostHeader", AppBlogPostHeader);
-    app.component("Alert", Alert);
   },
   Layout() {
-    return h(VoidZeroTheme.Layout, null, {
-      "layout-top": () => h(defineAsyncComponent(() => import("./components/Banner.vue"))),
-    });
+    return h(VoidZeroTheme.Layout, null, {});
   },
 } satisfies Theme;
