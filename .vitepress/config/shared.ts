95a57f807134c518330400b05b738901ac5a94a7
mdong1909 <mdong1909@users.noreply.github.com>
2025-12-29 15:45:14 +0800

    update og linking

--- .vitepress/config/shared.ts
+++ .vitepress/config/shared.ts
@@ -27,7 +27,7 @@ const head: HeadConfig[] = [
     "meta",
     {
       property: "og:image",
-      content: "https://cdn.jsdelivr.net/gh/oxc-project/oxc-assets/preview-universal.png",
+      content: "/og.jpg",
     },
   ],
   // Twitter (X)
@@ -37,7 +37,7 @@ const head: HeadConfig[] = [
     "meta",
     {
       name: "twitter:image",
-      content: "https://cdn.jsdelivr.net/gh/oxc-project/oxc-assets/preview-universal.png",
+      content: "/og.jpg",
     },
   ],
   // banner
