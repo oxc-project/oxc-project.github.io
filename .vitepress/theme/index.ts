082f2ba46a6a54df461815dfbdc8a9cb2c812054
mdong1909 <mdong1909@users.noreply.github.com>
2026-01-09 12:10:37 +0800

    refactor: consolidate overrides.css into style.css

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,7 +3,6 @@ import type { Theme } from "vitepress";
 import OxcTheme from "@voidzero-dev/vitepress-theme/src/oxc";
 import AppBlogPostHeader from "./components/AppBlogPostHeader.vue";
 import "./styles.css";
-import "./overrides.css";
 
 export default {
   extends: OxcTheme as unknown as any,
