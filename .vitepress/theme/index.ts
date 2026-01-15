38c09401a830528ed5ecba23f1af0b227e0e3a0a
Connor Shea <connor.james.shea@gmail.com>
2026-01-14 17:06:53 -0700

    Add back the removed Alert.vue component.
    This broke rule pages across the site (Alert is used to show the type-aware notice, default status, and fixable status on rules).

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,11 +4,13 @@ import OxcTheme from "@voidzero-dev/vitepress-theme/src/oxc";
 import AppBlogPostHeader from "./components/AppBlogPostHeader.vue";
 import AppBlogList from "./components/AppBlogList.vue";
 import RulesTable from "./components/RulesTable.vue";
+import Alert from "./components/Alert.vue";
 import "./styles.css";
 
 export default {
   extends: OxcTheme as unknown as any,
   async enhanceApp({ app }) {
+    app.component("Alert", Alert);
     app.component("AppBlogPostHeader", AppBlogPostHeader);
     app.component("AppBlogList", AppBlogList);
     app.component("RulesTable", RulesTable);
